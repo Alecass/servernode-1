269b1e29572277a57571391573dd9bdd74395c8c
webnicola <webnicola@gmail.com>
2019-04-16 10:42:08 +0200

    cambio porta webserver

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ const server = http.createServer(function (req, res) {
 })
 // in ascolto
 // server.listen(porta, url);
-server.listen(8080, '127.0.0.1');
+server.listen(8081, '127.0.0.1');
 
-console.log('Server running at http://127.0.0.1:8080/');
+console.log('Server running at http://127.0.0.1:8081/');
