6d3d84c07ca570b5d5798fe4aadc608d5afefe4c
webnicola <webnicola@gmail.com>
2019-04-16 11:35:58 +0200

    add html code

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const http = require('http');
 // comportamento web server con callback
 const server = http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write('Nome Cognome');
+  res.write('<h1>Nome Cognome</h1>');
   res.end();
 })
 // in ascolto
