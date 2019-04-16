f1d64abe36a6a1253a34e4115b26eaeefd177323
webnicola <webnicola@gmail.com>
2019-04-16 11:00:29 +0200

    cambio messaggio

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const http = require('http');
 // comportamento web server con callback
 const server = http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write('Ciao mondo!');
+  res.write('Nome Cognome');
   res.end();
 })
 // in ascolto
