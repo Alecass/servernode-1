d435764603091b87a8dfbc8028a77072f00eb207
webnicola <webnicola@gmail.com>
2019-04-19 12:15:03 +0200

    add module fs

--- index.js
+++ index.js
@@ -17,4 +17,17 @@ app.get('/books', function(req, res){
   })
 })
 
+app.get('/menu', function(req, res){
+  const filename = process.argv[3]
+  fs.readFile(filename, function(e, data) {
+    // 500 Internal Server Error
+    if (e) return res.sendStatus(500)
+    try {
+      menu = JSON.parse(data)
+    } catch (e) {
+      res.sendStatus(500)
+    }
+    res.json(menu)
+  })
+})
 app.listen(port)
