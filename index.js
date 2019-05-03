42634671a1e80145873f96cd34288ed1e141fbf6
webnicola <webnicola@gmail.com>
2019-05-03 13:38:08 +0200

    add got api

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const {settings} = require('./settings.js')
 const users = require('./routes/users')
 const personaggi = require('./routes/personaggi')
 let port = process.argv[2] || 8080
-
 app.use('/users', users)
 app.use('/personaggi', personaggi)
 
--- routes/personaggi.js
+++ routes/personaggi.js
@@ -1,28 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const characters = require('../data/characters')
 // QUery
 // chiedi http://localhost:7070/personaggi?colore=giallo&sesso=m
 router.get('/', (req, res) => {
   const query = req.query
-  let msg = 'Ciao'
-  if (query.key === '76512765127635126') {
-    msg = `${msg} Mario Rossi `
-    if (query.sesso === 'm') {
-      msg = `${msg} bel uomo`
-    }
-    if (query.colore) {
-      msg = `${msg} ${query.colore}`
-    }
-  } else {
-    msg = `Non sei autorizzato`
+  let personaggi = characters.characters
+  if ('lastname' in query) {
+    personaggi = personaggi.filter((personaggio) => {
+      return personaggio.lastname === query.lastname
+    })
+  }
+  if ('firstname' in query) {
+    personaggi = personaggi.filter((personaggio) => {
+      return personaggio.firstname === query.firstname
+    })
   }
 
-  res.send(msg)
+  res.send(personaggi)
 })
 
 // Params
-// http://localhost:7070/personaggi/9
-router.get('/:id', (req, res) => {
+//http://localhost:7070/personaggi/9
+router.get('/:id/', (req, res) => {
   const id = req.params.id
   console.log('ID: ', id)
   res.send(`personaggio id: ${id}`)
